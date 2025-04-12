3c4480774c9950ff2a47878b59e4cac3f97b0bf1
Harsh Jaiswal <157729350+HarshJaiswal02@users.noreply.github.com>
2025-04-12 19:55:23 +0530

    Update friend2.cpp

--- Friend/friend2.cpp
+++ Friend/friend2.cpp
@@ -37,3 +37,55 @@ int main()
     cout<< sum(obj1,obj2);   
     return 0;
 }
+
+// UPDATED CODE WITH SOME MORE DEPTH UNDERSTANDING
+#include <bits/stdc++.h>
+using namespace std;
+
+class B;
+
+class A
+{
+private:
+    int x;
+    friend int sum(A, B);
+    friend int sum(A *, B *);
+
+public:
+    A(int a)
+    {
+        x = a;
+    }
+};
+class B
+{
+private:
+    int x;
+    friend int sum(A, B);
+    friend int sum(A *, B *);
+
+public:
+    B(int b)
+    {
+        x = b;
+    }
+};
+
+int sum(A *ptrA, B *ptrB)
+{
+    return ptrA->x + ptrB->x;
+}
+
+int sum(A ptrA, B ptrB)
+{
+    return ptrA.x + ptrB.x;
+}
+
+int main()
+{
+    A obj1(2);
+    B obj2(3);
+
+    cout << sum(&obj1, &obj2);
+    cout << sum(obj1, obj2);
+}
